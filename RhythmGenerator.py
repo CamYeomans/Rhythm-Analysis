1808d7b9ea621d0414865d8970ab9a579cc49f94
Cameron J. Yeomans <cameron.yeomans@uvm.edu>
2019-03-05 13:58:24 -0700

    search feature now implimented

--- RhythmGenerator.py
+++ RhythmGenerator.py
@@ -1,3 +1,23 @@
+#A program used to find every possible matching rhythm
+#Written by Cameron Yeomans for Linear Algebra at the University of Utah 2019
+
+import re
+regex = "^[\+|\-]+$"
+
+print("Rhythm builder")
+print("use + to enter an eigth note")
+print("use - to enter an eigth rest")
+print("input must be 8 chars or less and contain only the chars + and -")
+
+error = True
+
+while(error):
+    searchRhythm = raw_input("Enter a valid rhythm: ")
+    valid = re.search(regex, searchRhythm)
+    if(len(searchRhythm) < 9 and valid):
+        error = False
+    if(searchRhythm == ""):
+        break
 
 
 rhythmsInBinary = []
@@ -7,7 +27,14 @@ for x in range(256):
     binaryVal = str(bin(x)[2:])
     while(len(binaryVal)!= 8):
         binaryVal = "0" + binaryVal
-    rhythmsInBinary.append(binaryVal)
+        
+    match = True
+    for y in range(len(searchRhythm)):
+        if((searchRhythm[y] == "+" and binaryVal[y] == "0") or (searchRhythm[y] == "-" and binaryVal[y] == "1")):
+            match = False
+    if(match):        
+        rhythmsInBinary.append(binaryVal)
+    match = True
 
 
 print(rhythmsInBinary)
